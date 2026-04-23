d1e2a82b5be3f8534a4870b320ea634a5777de54
JaneCCP <62127496+JaneCCP@users.noreply.github.com>
2026-04-23 19:21:09 +0800

    Initial commit

---
+++ root/usr/share/AdGuardHome/gfw2adg.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+LUA=/usr/bin/lua
+LUA_SCRIPT=/usr/share/AdGuardHome/gfw2adg.lua
+
+configpath=$(uci get AdGuardHome.AdGuardHome.configpath 2>/dev/null)
+if [ -z "$configpath" ] || [ ! -f "$configpath" ]; then
+    echo "please make a config first"
+    exit 1
+fi
+
+exec "$LUA" "$LUA_SCRIPT" --mode=upstream "$@"
